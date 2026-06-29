256e3d19c037cfadb78d280e95377d61bfe052fe
dahamci <wijesiriwardanadahamci@gmail.com>
2026-06-29 17:57:28 +0530

    initial commit-my portfolio

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Mobile menu toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+});
+
+// Close menu when a link is clicked
+navLinks.querySelectorAll('a').forEach(link => {
+  link.addEventListener('click', () => navLinks.classList.remove('open'));
+});
